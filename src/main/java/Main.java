65c901436a7a9d56b1e119ae40740432278bef89
kurogo-nguyen <52783990+kurogo-nguyen@users.noreply.github.com>
2021-06-20 16:08:02 +0700

    fix to run by cmd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,24 @@
 //import java.io.*;
+
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] argv) {
         /* Start the parser */
-        String file_path = "src/test/data/test.txt";
-//        if (argv.length == 0){
-//            System.out.println("Enter path to process:");
-//            Scanner scanner = new Scanner(System. in);
-//            file_path = scanner.nextLine();
-//        } else {
-//            file_path = argv[0];
-//        }
+        String file_path = "";
+        if (argv.length == 0) {
+            System.out.println("Enter path to process:");
+            Scanner scanner = new Scanner(System.in);
+            file_path = scanner.nextLine();
+        } else {
+            file_path = argv[0];
+        }
         try {
-            VCToken token;
             VC_Lexical lex = new VC_Lexical(new FileReader(file_path));
             parser p = new parser(lex);
             p.parse();
         } catch (Exception e) {
-            /* do cleanup here -- possibly rethrow e */
             e.printStackTrace();
         }
     }
